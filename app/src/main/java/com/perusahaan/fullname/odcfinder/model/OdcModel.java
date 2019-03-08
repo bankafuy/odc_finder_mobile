55ced07bffc18f829cf91925c425449d9a44a2b4
Full Name <khotibul@code.id>
2019-03-08 19:51:36 +0700

    update odc model.

--- app/src/main/java/com/perusahaan/fullname/odcfinder/model/OdcModel.java
+++ app/src/main/java/com/perusahaan/fullname/odcfinder/model/OdcModel.java
@@ -1,18 +1,48 @@
 package com.perusahaan.fullname.odcfinder.model;
 
+import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
  * Created by Full Name on 3/8/2019.
  */
 
-public class OdcModel {
+public class OdcModel implements Parcelable {
     private String namaOdc;
     private String kapasitas;
     private String datel;
     private String witel;
     private Float latitude;
     private Float longitude;
+
+    protected OdcModel(Parcel in) {
+        namaOdc = in.readString();
+        kapasitas = in.readString();
+        datel = in.readString();
+        witel = in.readString();
+        if (in.readByte() == 0) {
+            latitude = null;
+        } else {
+            latitude = in.readFloat();
+        }
+        if (in.readByte() == 0) {
+            longitude = null;
+        } else {
+            longitude = in.readFloat();
+        }
+    }
+
+    public static final Creator<OdcModel> CREATOR = new Creator<OdcModel>() {
+        @Override
+        public OdcModel createFromParcel(Parcel in) {
+            return new OdcModel(in);
+        }
+
+        @Override
+        public OdcModel[] newArray(int size) {
+            return new OdcModel[size];
+        }
+    };
 
     public String getNamaOdc() {
         return namaOdc;
@@ -50,5 +80,20 @@ public class OdcModel {
     }
     public void setLongitude(Float longitude) {
         this.longitude = longitude;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(namaOdc);
+        parcel.writeString(kapasitas);
+        parcel.writeString(datel);
+        parcel.writeString(witel);
+        parcel.writeFloat(latitude);
+        parcel.writeFloat(longitude);
     }
 }
